7717e9c8826d713032a67fa7005d678d8efc6c12
Philippe Boechat <63985012+phdev13@users.noreply.github.com>
2025-07-23 02:39:10 -0300

    Update HeroSection.tsx
    teste

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,4 +1,4 @@
-﻿import React, { useState, useCallback, useRef, useEffect } from 'react';
+import React, { useState, useCallback, useRef, useEffect } from 'react';
 import { UploadIcon } from './icons';
 
 // --- Constantes para melhor manutenção ---
